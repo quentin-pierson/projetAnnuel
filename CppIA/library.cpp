e99c860b91bd02dd0addb4d0599d0a1004fb193a
quentin pierson <quentinprs62@gmail.com>
2021-05-03 16:06:07 +0200

    add the library

---
+++ CppIA/library.cpp
@@ -0,0 +1,10 @@
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define DLLEXPORT extern "C" __declspec(dllexport)
+#else
+#define DLLEXPORT extern "C"
+#endif
+
+DLLEXPORT int toto() {
+    return 42;
+}
+
