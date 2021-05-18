9e3b187469180d51e876a55ec3c6c5a6c425015f
quentin pierson <quentinprs62@gmail.com>
2021-05-18 23:27:52 +0200

    add main.py

--- python/main.py
+++ python/main.py
@@ -3,8 +3,11 @@ import path_to_dll as path
 import matplotlib.pyplot as plt
 import numpy as np
 from linearModel import linearModel
+from mlp import MLPModel
 
 lm = linearModel()
+mlp = MLPModel()
+
 
 if __name__ == '__main__':
 
@@ -107,3 +110,58 @@ if __name__ == '__main__':
     plt.show()
 
     print("Je suis dead ", lm.destroy_linear_model(resultat2))
+
+    # ----------------------------------------------------------------------------------
+    #               Utilisation du MLP pour la classification
+    # ----------------------------------------------------------------------------------
+
+    print("Je suis la ")
+
+    dataset_inputs = [
+        [0, 0],
+        [1, 1],
+        [0, 1],
+        [1, 0],
+    ]
+
+    dataset_expected_outputs = [
+        -1,
+        -1,
+        1,
+        1,
+    ]
+    mlp.create_mlp_model([2, 2, 1])
+    #model = mlp.create_mlp_model([2, 2, 1])
+    test_dataset = [[x1 / 10, x2 / 10] for x1 in range(-10, 20) for x2 in range(-10, 20)]
+    colors = ["blue" if output >= 0 else "red" for output in dataset_expected_outputs]
+    print("j'ai passer la couleur")
+
+
+    #
+    # predicted_outputs = [lm.predict_mlp_model_classification(model, p)[0] for p in test_dataset]
+    # predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
+    # plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
+    # plt.scatter([p[0] for p in dataset_inputs], [p[1] for p in dataset_inputs], c=colors, s=200)
+    # plt.show()
+    #
+    # flattened_dataset_inputs = []
+    # for p in dataset_inputs:
+    #     flattened_dataset_inputs.append(p[0])
+    #     flattened_dataset_inputs.append(p[1])
+    #
+    # train_classification_stochastic_gradient_backpropagation_mlp_model(model,
+    #                                                                    flattened_dataset_inputs,
+    #                                                                    dataset_expected_outputs,
+    #                                                                    alpha=0.001,
+    #                                                                    iterations_count=100000)
+    #
+    # predicted_outputs = [predict_mlp_model_classification(model, p)[0] for p in test_dataset]
+    # predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
+    # plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
+    # plt.scatter([p[0] for p in dataset_inputs], [p[1] for p in dataset_inputs], c=colors, s=200)
+    # plt.show()
+    #
+    # flattened_dataset_inputs = []
+    # for p in dataset_inputs:
+    #     flattened_dataset_inputs.append(p[0])
+    #     flattened_dataset_inputs.append(p[1])
