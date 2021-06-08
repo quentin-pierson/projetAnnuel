d3eb48acb5c52eb4dc9e335aeb40fa1f62f824d0
antdbs <antoinedubois14@gmail.com>
2021-06-08 18:22:51 +0200

    Update web app

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-import json
-
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 
 ########################################################################
 ##############################   TASK   ################################
@@ -16,14 +15,34 @@ app = Flask(__name__)
 ########################################################################
 ##############################   CODE   ################################
 ########################################################################
-@app.route('/')
-def home():
-    return render_template("index.html")
+
+
+
+UPLOAD_FOLDER = 'C:/Users/Antoine/Desktop/Projet_annuel/projetAnnuel/flaskApp'
+ALLOWED_EXTENSIONS = ['.png', '.pdf']
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ALLOWED_EXTENSIONS
+app.config['UPLOAD_PATH'] = 'upload'
 
 @app.route('/')
-def result():
-    return render_template("")
+def index():
+    return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    return "metal"
+
+    # uploaded_file = request.files['file']
+    # filename = secure_filename(uploaded_file.filename)
+    # if filename != '':
+    #     file_ext = os.path.splitext(filename)[1]
+    #     if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+    #         abort(400)
+    #     uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    # return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
