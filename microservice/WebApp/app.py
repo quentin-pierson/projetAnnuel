5f65e5020928f91b343a176cdf6a9a9015b9cf87
antdbs <antoinedubois14@gmail.com>
2021-07-27 01:01:19 +0200

    Update microservice

--- microservice/WebApp/app.py
+++ microservice/WebApp/app.py
@@ -5,7 +5,19 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def home():
+    
+    pathPict = "C:/Users/Antoine/Desktop/Projet_annuel/projetAnnuel/data/presentation"
+    onlyPicture = [p for p in listdir(pathPict) if isfile(join(pathPict, p))]
+
+    pathModel = "C:/Users/Antoine/Desktop/Projet_annuel/projetAnnuel/Save"
+    onlyModel = [m for m in listdir(pathModel) if isfile(join(pathModel, m))]
+
+    return render_template('home.html', pict=onlyPicture, model=onlyModel)
+
+@app.route('/default')
 def savedModel():
     mypath = "C:/Users/Antoine/Desktop/Projet_annuel/projetAnnuel/Save"
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
